d0d2c23bc522eca53e105a50b5d1b84a3f60e378
Arun Rao <yarun1995.ar@gmail.com>
2024-06-19 00:31:31 +0530

    updated feature and create responsive

--- js/game.js
+++ js/game.js
@@ -8,104 +8,174 @@ let level = 0;
 
 
 
-// Game Start
+// GAME START
 
-let levelTitle = document.querySelector("#level-title")
+let levelTitle = document.querySelector("#level-title");
 
 document.addEventListener("keypress", () => {
-	if (!gameStarted) {
-		gameSequence();
-		gameStarted = true;
-	}
+
+    if (!gameStarted) {
+        gameSequence();
+        gameStarted = true;
+    }
+
 });
 
 
 
-//Create User Click Pattern Sequence to Check it with Game Sequence
+
+// RESPONSIVE JS
+
+// DEFINE MEDIA QUERIES
+
+let smallScreen = window.matchMedia('(max-width: 420px)');
+
+function myRespFunc() {
+
+	// IF MEDIA QUERY MATCHES
+
+    if (smallScreen.matches) {
+
+        if (!gameStarted) {
+            gameSequence();
+            gameStarted = true;
+        }
+    }
+}
+
+// CALL LISTENER FUNCTION ON JS LOAD
+
+myRespFunc();
+
+// ATTACH LISTENER FUNCTION ON STATE CHANGES
+
+smallScreen.addEventListener("change", myRespFunc);
+
+
+
+
+// CREATE USER CLICK PATTERN SEQUENCE TO CHECK IT WITH GAME SEQUENCE
 
 for (let i = 0; i < 4; i++) {
-	document.querySelectorAll(".btn")[i].addEventListener("click", function() {
-		let userChosenColour = this.attributes.id.value;
-		userClickPattern.push(userChosenColour);
 
-		playSound(userChosenColour);
-		pressButtonAnimation(userChosenColour);
-		checkGameSequencePattern(userClickPattern.length - 1);
-	});
+    document.querySelectorAll(".btn")[i].addEventListener("click", function () {
+    
+	let userChosenColour = this.attributes.id.value;
+    
+	userClickPattern.push(userChosenColour);
+
+        playSound(userChosenColour);
+        pressButtonAnimation(userChosenColour);
+        checkGameSequencePattern(userClickPattern.length - 1);
+
+    });
 }
 
 
 
-//Create Pattern Sequence for Game
+
+// CREATE PATTERN SEQUENCE FOR GAME
 
 function gameSequence() {
-	userClickPattern = [];
-	level++;
-	levelTitle.textContent = "Level " + level;
-	let randomNumber = Math.floor(Math.random() * 4);
-	let randomChosenColour = button[randomNumber];
-	gamePattern.push(randomChosenColour);
 
-	document.querySelector("#" + randomChosenColour).classList.add("fadeInOut");
-	playSound(randomChosenColour);
+    userClickPattern = [];
 
-	setTimeout(function() {
-		document.querySelector("#" + randomChosenColour).classList.remove("fadeInOut");
-	}, 200);
+    level++;
+
+    levelTitle.textContent = "Level " + level;
+
+    let randomNumber = Math.floor(Math.random() * 4);
+
+    let randomChosenColour = button[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    document.querySelector("#" + randomChosenColour).classList.add("fadeInOut");
+
+    playSound(randomChosenColour);
+
+    setTimeout(function () {
+        document.querySelector("#" + randomChosenColour).classList.remove("fadeInOut");
+    }, 200);
 }
 
 
 
-// Button Sound Creation
+
+// BUTTON SOUND CREATION
 
 function playSound(name) {
-	var audio = new Audio("sounds/" + name + ".mp3");
-	audio.play();
+
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+
 }
 
 
 
-// Check Game pattern Match the user click Pattern
+
+// CHECK GAME PATTERN MATCH THE USER CLICK PATTERN
 
 function checkGameSequencePattern(currentLevel) {
-	if (gamePattern[currentLevel] === userClickPattern[currentLevel]) {
-		if (userClickPattern.length === gamePattern.length) {
-			setTimeout(function() {
-				gameSequence();
-			}, 1000);
-		}
 
-	} else {
-		playSound("wrong");
-		document.querySelector("body").classList.add("game-over");
-		levelTitle.textContent = "Game Over! " + "Press Any key to Restart the Game";
+    if (gamePattern[currentLevel] === userClickPattern[currentLevel]) {
 
-		setTimeout(function() {
-			document.querySelector("body").classList.remove("game-over");
-		}, 200);
+        if (userClickPattern.length === gamePattern.length) {
 
-		startOver();
+            setTimeout(function () {
+                gameSequence();
+            }, 1000);
 
-	}
+        }
+
+    } 
+	
+	else {
+
+        playSound("wrong");
+
+        document.querySelector("body").classList.add("game-over");
+
+        if (smallScreen.matches) {	
+            levelTitle.textContent = "Game Over! Refresh Page to Restart the Game";
+        } 
+		
+		else {
+            levelTitle.textContent = "Game Over! Press any key to Restart the Game";
+        }
+
+        setTimeout(function () {
+            document.querySelector("body").classList.remove("game-over");
+        }, 200);
+
+        startOver();
+    }
 }
 
 
 
-// Animate Button When Pressed
+
+// ANIMATE BUTTON WHEN PRESSED
 
 function pressButtonAnimation(currentColor) {
-	document.querySelector("#" + currentColor).classList.add("pressed");
-	setTimeout(function() {
-		document.querySelector("#" + currentColor).classList.remove("pressed");
-	}, 100);
+
+    document.querySelector("#" + currentColor).classList.add("pressed");
+
+    setTimeout(function () {
+        document.querySelector("#" + currentColor).classList.remove("pressed");
+    }, 100);
+
 }
 
 
 
-// Game Restart on keypress if Pattern is wrong
+
+// GAME RESTART ON KEYPRESS IF PATTERN IS WRONG
 
 function startOver() {
-	level = 0;
-	gamePattern = [];
-	gameStarted = false;
+
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+	
 }
